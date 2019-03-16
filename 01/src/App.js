5d833e90799c34217f921909c1b838a619ba69ac
Adam Romański <adam.romanski@netguru.pl>
2019-03-16 23:11:09 +0100

    01.3-finish

--- 01/src/App.js
+++ 01/src/App.js
@@ -1,25 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        kkErat diam diam ipsum kasd sit dolore no est, lorem ipsum sit et et. Est kasd sadipscing est voluptua sed erat erat. Sea elitr dolores et duo stet accusam tempor, eirmod at takimata magna sit ipsum consetetur, sanctus ipsum sit dolores lorem. Takimata takimata tempor et elitr duo gubergren sadipscing..
+      </a>
+    </header>
+  </div>
+);
 
 export default App;
