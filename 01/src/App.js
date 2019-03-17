08b9c5142f49ae87de4bbfb0fdd3e4819bc4a7e9
Adam Romański <adam.romanski@netguru.pl>
2019-03-17 22:58:46 +0100

    01.5

---
+++ 01/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+        alt="roman"
+      >
+        kkErat diam diam ipsum kasd sit dolore no est, lorem ipsum sit et et. Est kasd sadipscing
+        est voluptua sed erat erat. Sea elitr dolores et duo stet accusam tempor, eirmod at takimata
+        magna sit ipsum consetetur, sanctus ipsum sit dolores lorem. Takimata takimata tempor et
+        elitr duo gubergren sadipscing..
+      </a>
+    </header>
+  </div>
+);
+
+export default App;
