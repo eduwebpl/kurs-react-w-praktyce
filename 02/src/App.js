d66affb02d03cca89bbdd89f4fe8180455fd038c
Adam Romański <adam.romanski@netguru.pl>
2019-03-21 19:12:54 +0100

    02.3

---
+++ 02/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+import logo from './logo.svg';
+import './App.css';
+
+const MyButton = styled.button`
+  border: 2px solid blue;
+  padding: 50px;
+`;
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <MyButton>This is my button</MyButton>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+        alt="roman"
+      >
+        kkErat diam diam ipsum kasd sit dolore no est, lorem ipsum sit et et. Est kasd sadipscing
+        est voluptua sed erat erat. Sea elitr dolores et duo stet accusam tempor, eirmod at takimata
+        magna sit ipsum consetetur, sanctus ipsum sit dolores lorem. Takimata takimata tempor et
+        elitr duo gubergren sadipscing..
+      </a>
+    </header>
+  </div>
+);
+
+export default App;
