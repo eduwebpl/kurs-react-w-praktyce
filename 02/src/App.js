44401e5ab54fbabba50131be157ad5265e69002b
Adam Romański <adam.romanski@netguru.pl>
2019-03-21 18:43:24 +0100

    02.2-finish

--- 02/src/App.js
+++ 02/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
 import logo from './logo.svg';
 import './App.css';
+
+const MyButton = styled.button`
+  border: 2px solid blue;
+  padding: 50px;
+`;
 
 const App = () => (
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
+      <MyButton>This is my button</MyButton>
       <a
         className="App-link"
         href="https://reactjs.org"
