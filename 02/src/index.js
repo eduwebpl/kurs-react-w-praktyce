7411a13335fa24dbbd9df17ddd21ce7aa7c12733
Adam Romański <adam.romanski@netguru.pl>
2019-03-21 22:00:59 +0100

    02.3-finish

--- 02/src/index.js
+++ 02/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Root from 'views/Root/Root';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Root />, document.getElementById('root'));
---
+++ 02/src/views/Root/Root.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from 'components/Button/Button';
+
+const Root = () => (
+  <div>
+    <h1>Hello Roman</h1>
+    <Button>Helo helo</Button>
+  </div>
+);
+
+export default Root;
