fc67b709d0836bcc2594aadb01f5040ed91f5b16
Adam Romański <adam.romanski@netguru.pl>
2019-03-23 11:56:07 +0100

    02.5-finish

--- 02/src/components/Button/Button.js
+++ 02/src/components/Button/Button.js
@@ -1,16 +1,25 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Button = styled.button`
   padding: 0;
-  background-color: ${({ secondary }) => (secondary ? '#E6E6E6' : '#FFD82B')};
-  width: ${({ secondary }) => (secondary ? '105px' : '220px')};
-  height: ${({ secondary }) => (secondary ? '30px' : '47px')};
+  background-color: #e6e6e6;
+  width: ${({ width }) => width || '220px'};
+  height: 47px;
   border: none;
   border-radius: 50px;
   font-family: 'Montserrat';
   font-weight: 600;
-  font-size: ${({ secondary }) => (secondary ? '10px' : '16px')};
+  font-size: 16px;
   text-transform: uppercase;
+
+  ${({ secondary }) =>
+    secondary &&
+    css`
+      background-color: #ffd82b;
+      width: 105px;
+      height: 30px;
+      font-size: 10px;
+    `}
 `;
 
 export default Button;
--- 02/src/views/Root/Root.js
+++ 02/src/views/Root/Root.js
@@ -4,7 +4,7 @@ import Button from 'components/Button/Button';
 const Root = () => (
   <div>
     <h1>Hello Roman</h1>
-    <Button>Close / Save</Button>
+    <Button width="20px">Close / Save</Button>
     <Button secondary>Remove</Button>
   </div>
 );
