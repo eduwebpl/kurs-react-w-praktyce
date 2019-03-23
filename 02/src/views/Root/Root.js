9d05bb37589ad98279fef43886512b3936d62662
Adam Romański <adam.romanski@netguru.pl>
2019-03-23 20:13:54 +0100

    02.6-finish

--- 02/src/views/Root/Root.js
+++ 02/src/views/Root/Root.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Button from 'components/Button/Button';
+import GlobalStyle from 'theme/GlobalStyle';
 
 const Root = () => (
   <div>
+    <GlobalStyle />
     <h1>Hello Roman</h1>
-    <Button width="20px">Close / Save</Button>
+    <Button>Close / Save</Button>
     <Button secondary>Remove</Button>
   </div>
 );
