ddbc2dc373c6f5912914cee9990e75199d27b903
Adam Romański <adam.romanski@netguru.pl>
2019-03-23 21:54:15 +0100

    03.3-finish

--- 03/src/components/Button/Button.js
+++ 03/src/components/Button/Button.js
@@ -2,7 +2,7 @@ import styled, { css } from 'styled-components';
 
 const Button = styled.button`
   padding: 0;
-  background-color: #ffd82b;
+  background-color: ${({ color }) => color || 'hsl(49, 100%, 58%)'};
   width: ${({ width }) => width || '220px'};
   height: 47px;
   border: none;
@@ -15,7 +15,7 @@ const Button = styled.button`
   ${({ secondary }) =>
     secondary &&
     css`
-      background-color: #e6e6e6;
+      background-color: hsl(0, 0%, 90%);
       width: 105px;
       height: 30px;
       font-size: 10px;
