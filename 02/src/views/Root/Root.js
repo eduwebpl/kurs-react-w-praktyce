1c80953a9ecf40aa995318b38a56ee6542aa5716
Adam Romański <adam.romanski@netguru.pl>
2019-03-23 11:56:53 +0100

    02.6

---
+++ 02/src/views/Root/Root.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from 'components/Button/Button';
+
+const Root = () => (
+  <div>
+    <h1>Hello Roman</h1>
+    <Button width="20px">Close / Save</Button>
+    <Button secondary>Remove</Button>
+  </div>
+);
+
+export default Root;
