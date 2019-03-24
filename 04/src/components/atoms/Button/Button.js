7b2702c3078395a53638477e96c9908cba2f674a
Adam Romański <adam.romanski@netguru.pl>
2019-03-24 23:40:33 +0100

    04

---
+++ 04/src/components/atoms/Button/Button.js
@@ -0,0 +1,25 @@
+import styled, { css } from 'styled-components';
+
+const Button = styled.button`
+  padding: 0;
+  background-color: ${({ theme }) => theme.primary};
+  width: 220px;
+  height: 47px;
+  border: none;
+  border-radius: 50px;
+  font-family: 'Montserrat';
+  font-weight: 600;
+  font-size: 16px;
+  text-transform: uppercase;
+
+  ${({ secondary }) =>
+    secondary &&
+    css`
+      background-color: hsl(0, 0%, 90%);
+      width: 105px;
+      height: 30px;
+      font-size: 10px;
+    `}
+`;
+
+export default Button;
