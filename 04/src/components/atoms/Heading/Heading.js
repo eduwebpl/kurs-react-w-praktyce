d10fa69b50871066dc523a55036bc5bdad7ac3b3
Adam Romański <adam.romanski@netguru.pl>
2019-03-30 15:13:54 +0100

    04.5

---
+++ 04/src/components/atoms/Heading/Heading.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Heading = styled.h1`
+  font-size: ${({ theme, big }) => (big ? theme.fontSize.xl : theme.fontSize.l)};
+  font-weight: ${({ theme }) => theme.bold};
+`;
+
+export default Heading;
