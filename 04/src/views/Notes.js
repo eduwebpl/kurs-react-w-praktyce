9e57c2a1345f7175c4e1e1860f2015dc5d002dbf
Adam Romański <adam.romanski@netguru.pl>
2019-03-31 11:14:50 +0200

    04.9-finish

--- 04/src/views/Notes.js
+++ 04/src/views/Notes.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import UserPageTemplate from 'templates/UserPageTemplate';
+import Card from 'components/molecules/Card/Card';
 
 const Notes = () => (
   <UserPageTemplate pageType="note">
-    <h1>Notes view</h1>
+    <Card cardType="note" />
+    <Card cardType="note" />
+    <Card cardType="note" />
+    <Card cardType="note" />
+    <Card cardType="note" />
+    <Card cardType="note" />
   </UserPageTemplate>
 );
 
