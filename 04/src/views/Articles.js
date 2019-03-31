658d30accccf60cae48db7a4b6a1143f8ee2a996
Adam Romański <adam.romanski@netguru.pl>
2019-03-31 10:31:48 +0200

    04.9

---
+++ 04/src/views/Articles.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import UserPageTemplate from 'templates/UserPageTemplate';
+
+const Articles = () => (
+  <UserPageTemplate pageType="article">
+    <h1>Articles view</h1>
+  </UserPageTemplate>
+);
+
+export default Articles;
---
+++ 04/src/views/Notes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import UserPageTemplate from 'templates/UserPageTemplate';
+
+const Notes = () => (
+  <UserPageTemplate pageType="note">
+    <h1>Notes view</h1>
+  </UserPageTemplate>
+);
+
+export default Notes;
