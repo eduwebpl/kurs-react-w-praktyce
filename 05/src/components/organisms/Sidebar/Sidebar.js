0bd571a5d92327e1d68f23afa5fdcdf2d0e79024
Adam Romański <adam.romanski@netguru.pl>
2019-04-02 22:43:24 +0200

    05.2

---
+++ 05/src/components/organisms/Sidebar/Sidebar.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import ButtonIcon from 'components/atoms/ButtonIcon/ButtonIcon';
+import bulbIcon from 'assets/icons/bulb.svg';
+import logoutIcon from 'assets/icons/logout.svg';
+import penIcon from 'assets/icons/pen.svg';
+import twitterIcon from 'assets/icons/twitter.svg';
+import logoIcon from 'assets/icons/logo.svg';
+
+const StyledWrapper = styled.nav`
+  position: fixed;
+  left: 0;
+  top: 0;
+  padding: 25px 0;
+  width: 150px;
+  height: 100vh;
+  background-color: ${({ activeColor, theme }) => (activeColor ? theme[activeColor] : theme.note)};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const StyledLogoLink = styled(NavLink)`
+  display: block;
+  width: 67px;
+  height: 67px;
+  background-image: url(${logoIcon});
+  background-repeat: no-repeat;
+  background-position: 50% 50%;
+  background-size: 80%;
+  border: none;
+  margin-bottom: 10vh;
+`;
+
+const StyledLogoutButton = styled(ButtonIcon)`
+  margin-top: auto;
+`;
+
+const StyledLinksList = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+`;
+
+const Sidebar = ({ pageType }) => (
+  <StyledWrapper activeColor={pageType}>
+    <StyledLogoLink to="/" />
+    <StyledLinksList>
+      <li>
+        <ButtonIcon as={NavLink} to="/notes" icon={penIcon} activeclass="active" />
+      </li>
+      <li>
+        <ButtonIcon as={NavLink} to="/twitters" icon={twitterIcon} activeclass="active" />
+      </li>
+      <li>
+        <ButtonIcon as={NavLink} to="/articles" icon={bulbIcon} activeclass="active" />
+      </li>
+    </StyledLinksList>
+    <StyledLogoutButton as={NavLink} to="/login" icon={logoutIcon} />
+  </StyledWrapper>
+);
+
+Sidebar.propTypes = {
+  pageType: PropTypes.oneOf(['notes', 'twitters', 'articles']),
+};
+
+Sidebar.defaultProps = {
+  pageType: 'notes',
+};
+
+export default Sidebar;
