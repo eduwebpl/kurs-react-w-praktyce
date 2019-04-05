2cfff91a67edc1c7ee337fff12416ebe7309d186
Adam Romański <adam.romanski@netguru.pl>
2019-04-05 21:23:30 +0200

    05.8

---
+++ 05/src/templates/GridTemplate.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import UserPageTemplate from 'templates/UserPageTemplate';
+import Input from 'components/atoms/Input/Input';
+import Heading from 'components/atoms/Heading/Heading';
+import Paragraph from 'components/atoms/Paragraph/Paragraph';
+
+const StyledWrapper = styled.div`
+  padding: 25px 150px 25px 70px;
+`;
+
+const StyledGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 85px;
+
+  @media (max-width: 1500px) {
+    grid-gap: 45px;
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (max-width: 1100px) {
+    grid-template-columns: 1fr;
+  }
+`;
+
+const StyledPageHeader = styled.div`
+  margin: 25px 0 50px 0;
+`;
+
+const StyledHeading = styled(Heading)`
+  margin: 25px 0 0 0;
+
+  ::first-letter {
+    text-transform: uppercase;
+  }
+`;
+
+const StyledParagraph = styled(Paragraph)`
+  margin: 0;
+  font-weight: ${({ theme }) => theme.bold};
+`;
+
+const GridTemplate = ({ children, pageType }) => (
+  <UserPageTemplate pageType={pageType}>
+    <StyledWrapper>
+      <StyledPageHeader>
+        <Input search placeholder="Search" />
+        <StyledHeading big as="h1">
+          {pageType}
+        </StyledHeading>
+        <StyledParagraph>6 {pageType}</StyledParagraph>
+      </StyledPageHeader>
+      <StyledGrid>{children}</StyledGrid>
+    </StyledWrapper>
+  </UserPageTemplate>
+);
+
+GridTemplate.propTypes = {
+  children: PropTypes.arrayOf(PropTypes.object).isRequired,
+  pageType: PropTypes.oneOf(['notes', 'twitters', 'articles']),
+};
+
+GridTemplate.defaultProps = {
+  pageType: 'notes',
+};
+
+export default GridTemplate;
