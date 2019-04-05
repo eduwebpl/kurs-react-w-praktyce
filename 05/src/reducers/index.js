c6d69ec834d1f5285b1b96b2f897c26c10ece5f1
Adam Romański <adam.romanski@netguru.pl>
2019-04-05 21:23:01 +0200

    05.7-finish

--- 05/src/reducers/index.js
+++ 05/src/reducers/index.js
@@ -54,9 +54,18 @@ const initialState = {
   ],
 };
 
-// eslint-disable-next-line
 const rootReducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case 'REMOVE_ITEM':
+      return {
+        ...state,
+        [action.payload.itemType]: [
+          ...state[action.payload.itemType].filter(item => item.id !== action.payload.id),
+        ],
+      };
+    default:
+      return state;
+  }
 };
 
 export default rootReducer;
