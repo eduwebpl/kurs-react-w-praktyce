b39a74170dbd3f0b5169fa8d8a675faea40377ca
Adam Romański <adam.romanski@netguru.pl>
2019-04-06 13:31:22 +0200

    05.14

---
+++ 05/src/templates/MainTemplate.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from 'theme/GlobalStyle';
+import PageContext from 'context';
+import { theme } from 'theme/mainTheme';
+
+class MainTemplate extends Component {
+  state = {
+    pageType: 'notes',
+  };
+
+  componentDidMount() {
+    this.setCurrentPage();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    this.setCurrentPage(prevState);
+  }
+
+  setCurrentPage = (prevState = '') => {
+    const pageTypes = ['twitters', 'articles', 'notes'];
+    const {
+      location: { pathname },
+    } = this.props;
+
+    const [currentPage] = pageTypes.filter(page => pathname.includes(page));
+
+    if (prevState.pageType !== currentPage) {
+      this.setState({ pageType: currentPage });
+    }
+  };
+
+  render() {
+    const { children } = this.props;
+    const { pageType } = this.state;
+
+    return (
+      <div>
+        <PageContext.Provider value={pageType}>
+          <GlobalStyle />
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </PageContext.Provider>
+      </div>
+    );
+  }
+}
+
+MainTemplate.propTypes = {
+  children: PropTypes.element.isRequired,
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default withRouter(MainTemplate);
