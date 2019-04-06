f4771f63e0a88295a963e34dc6bd4c4b80093dc3
Adam Romański <adam.romanski@netguru.pl>
2019-04-06 14:46:43 +0200

    05.15

---
+++ 05/src/actions/index.js
@@ -0,0 +1,27 @@
+export const removeItem = (itemType, id) => {
+  return {
+    type: 'REMOVE_ITEM',
+    payload: {
+      itemType,
+      id,
+    },
+  };
+};
+
+export const addItem = (itemType, itemContent) => {
+  const getId = () =>
+    `_${Math.random()
+      .toString(36)
+      .substr(2, 9)}`;
+
+  return {
+    type: 'ADD_ITEM',
+    payload: {
+      itemType,
+      item: {
+        id: getId(),
+        ...itemContent,
+      },
+    },
+  };
+};
---
+++ 05/src/routes/index.js
@@ -0,0 +1,10 @@
+export const routes = {
+  home: '/',
+  notes: '/notes',
+  note: '/notes/details/:id',
+  twitters: '/twitters',
+  twitter: '/twitters/details/:id',
+  articles: '/articles',
+  article: '/articles/details/:id',
+  login: '/login',
+};
