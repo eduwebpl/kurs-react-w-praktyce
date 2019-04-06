7ae957d50f1eb56dcef94b83f3b1590ce589e4c1
Adam Romański <adam.romanski@netguru.pl>
2019-04-06 20:15:12 +0200

    06.2

---
+++ 06/src/actions/index.js
@@ -0,0 +1,30 @@
+export const removeItem = (itemType, id) => {
+  return {
+    type: 'REMOVE_ITEM',
+    payload: {
+      itemType,
+      id,
+    },
+  };
+};
+
+export const addItem = (itemType, itemContent) => {
+  const getId = () =>
+    `_${Math.random()
+      .toString(36)
+      .substr(2, 9)}`;
+
+  // dispatch({type: 'ADD_ITEM_REQUEST'})
+  // return axios('blabla', 'getState().').then(response => dispatch({ type: 'ADD_ITEM_SUCCESS', response})).catch()
+
+  return {
+    type: 'ADD_ITEM',
+    payload: {
+      itemType,
+      item: {
+        id: getId(),
+        ...itemContent,
+      },
+    },
+  };
+};
