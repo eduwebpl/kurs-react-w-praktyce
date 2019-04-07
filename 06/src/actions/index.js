c201fdb960b5c9e40fd71b94cc47f8e75b3b03df
Adam Romański <adam.romanski@netguru.pl>
2019-04-07 12:45:54 +0200

    06.10

---
+++ 06/src/actions/index.js
@@ -0,0 +1,78 @@
+import axios from 'axios';
+
+export const REMOVE_ITEM = 'REMOVE_ITEM';
+export const ADD_ITEM = 'ADD_ITEM';
+
+export const AUTH_REQUEST = 'AUTH_REQUEST';
+export const AUTH_SUCCESS = 'AUTH_SUCCESS';
+export const AUTH_FAILURE = 'AUTH_FAILURE';
+
+export const FETCH_REQUEST = 'FETCH_REQUEST';
+export const FETCH_SUCCESS = 'FETCH_SUCCESS';
+export const FETCH_FAILURE = 'FETCH_FAILURE';
+
+export const authenticate = (username, password) => dispatch => {
+  dispatch({ type: AUTH_REQUEST });
+
+  return axios
+    .post('http://localhost:9000/api/user/login', {
+      username,
+      password,
+    })
+    .then(payload => {
+      console.log(payload);
+      dispatch({ type: AUTH_SUCCESS, payload });
+    })
+    .catch(err => {
+      console.log(err);
+      dispatch({ type: AUTH_FAILURE });
+    });
+};
+
+export const fetchItems = itemType => (dispatch, getState) => {
+  dispatch({ type: FETCH_REQUEST });
+
+  return axios
+    .get('http://localhost:9000/api/notes/type', {
+      params: {
+        type: itemType,
+        userID: getState().userID,
+      },
+    })
+    .then(({ data }) => {
+      dispatch({
+        type: FETCH_SUCCESS,
+        payload: {
+          data,
+          itemType,
+        },
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      dispatch({ type: FETCH_FAILURE });
+    });
+};
+
+export const removeItem = (itemType, id) => {
+  return {
+    type: REMOVE_ITEM,
+    payload: {
+      itemType,
+      id,
+    },
+  };
+};
+
+export const addItem = (itemType, itemContent) => {
+  return {
+    type: ADD_ITEM,
+    payload: {
+      itemType,
+      item: {
+        id: '',
+        ...itemContent,
+      },
+    },
+  };
+};
