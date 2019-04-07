d9ca0d01b3a2214ba4bcb83bcab5895644ce708b
Adam Romański <adam.romanski@netguru.pl>
2019-04-07 12:45:09 +0200

    06.9-finish

--- 06/src/views/DetailsPage.js
+++ 06/src/views/DetailsPage.js
@@ -1,36 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import DetailsTemplate from 'templates/DetailsTemplate';
 import withContext from 'hoc/withContext';
 import { connect } from 'react-redux';
+import axios from 'axios';
 
-const dummyArticle = {
-  id: 1,
-  title: 'Wake me up when Vue ends',
-  content:
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
-  twitterName: 'hello_roman',
-  articleUrl: 'https://youtube.com/helloroman',
-};
+class DetailsPage extends Component {
+  state = {
+    activeItem: {
+      title: '',
+      content: '',
+      articleUrl: '',
+      twitterName: '',
+    },
+  };
 
-const DetailsPage = ({ activeItem }) => {
-  const [item] = activeItem;
+  componentDidMount() {
+    if (this.props.activeItem) {
+      const [activeItem] = this.props.activeItem;
+      this.setState({ activeItem });
+    } else {
+      const { id } = this.props.match.params;
+      axios
+        .get(`http://localhost:9000/api/note/${id}`)
+        .then(({ data }) => {
+          this.setState({ activeItem: data });
+        })
+        .catch(err => console.log(err));
+    }
+  }
 
-  return (
-    <DetailsTemplate
-      title={item.title}
-      content={item.content}
-      articleUrl={item.articleUrl}
-      twitterName={item.twitterName}
-    />
-  );
-};
+  render() {
+    const { activeItem } = this.state;
+
+    return (
+      <DetailsTemplate
+        title={activeItem.title}
+        content={activeItem.content}
+        articleUrl={activeItem.articleUrl}
+        twitterName={activeItem.twitterName}
+      />
+    );
+  }
+}
 
 const mapStateToProps = (state, ownProps) => {
-  console.log(ownProps);
-
-  return {
-    activeItem: state[ownProps.pageContext].filter(item => item._id === ownProps.match.params.id),
-  };
+  if (state[ownProps.pageContext]) {
+    return {
+      activeItem: state[ownProps.pageContext].filter(item => item._id === ownProps.match.params.id),
+    };
+  }
+  return {};
 };
 
 export default withContext(connect(mapStateToProps)(DetailsPage));
