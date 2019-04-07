c91cdba80f8e13dadbd7622b7b18749462c2daf2
Adam Romański <adam.romanski@netguru.pl>
2019-04-07 13:29:27 +0200

    06.11

---
+++ 06/src/reducers/index.js
@@ -0,0 +1,52 @@
+import {
+  ADD_ITEM_REQUEST,
+  ADD_ITEM_SUCCESS,
+  ADD_ITEM_FAILURE,
+  REMOVE_ITEM_REQUEST,
+  REMOVE_ITEM_SUCCESS,
+  AUTH_SUCCESS,
+  FETCH_REQUEST,
+  FETCH_SUCCESS,
+} from 'actions';
+
+const initialState = {
+  userID: '5ca8f00a097c3394e62f64ab',
+  isLoading: false,
+};
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case FETCH_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        [action.payload.itemType]: [...action.payload.data],
+      };
+    case AUTH_SUCCESS:
+      return {
+        ...state,
+        userID: action.payload.data._id,
+      };
+    case ADD_ITEM_SUCCESS:
+      return {
+        ...state,
+        [action.payload.itemType]: [...state[action.payload.itemType], action.payload.item],
+      };
+    case REMOVE_ITEM_SUCCESS:
+      return {
+        ...state,
+        [action.payload.itemType]: [
+          ...state[action.payload.itemType].filter(item => item._id !== action.payload.id),
+        ],
+      };
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
